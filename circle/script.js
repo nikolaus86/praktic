1e0875a099ec1aac60a3262d1a9a82a4ab7d7828
w0lf <n_khvorov@mail.ru>
2023-07-13 23:49:43 +0500

    pervi

---
+++ circle/script.js
@@ -0,0 +1,50 @@
+const canvas = document.getElementById('canvas1');
+const file = document.getElementById('fileupload');
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+const ctx = canvas.getContext('2d');
+let audioSource;
+let analyser;
+
+file.addEventListener('change', function(){
+    const files = this.files;
+    const audio1 = document.getElementById('audio1');
+    audio1.src = URL.createObjectURL(files[0]);
+    audio1.load();
+    const audioContext = new (window.AudioContext)();
+    audio1.play();
+    audioSource = audioContext.createMediaElementSource(audio1);
+    analyser = audioContext.createAnalyser();
+    audioSource.connect(analyser);
+    analyser.connect(audioContext.destination);
+    analyser.fftSize = 2048;
+    const bufferLength = analyser.frequencyBinCount;
+    const dataArray = new Uint8Array(bufferLength);
+
+    const barWidth = canvas.width/bufferLength*1.5;
+    let barHeight;
+    let x;
+
+    function animate(){
+        x = 0;
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        analyser.getByteFrequencyData(dataArray);
+        drawVisualizer(bufferLength, x, barWidth, barHeight, dataArray);
+        requestAnimationFrame(animate);
+    }
+    animate();
+})
+
+function drawVisualizer(bufferLength, x, barWidth, barHeight, dataArray){
+    for (let i = 0; i < bufferLength; i++){
+        barHeight = dataArray[i] * 1.5;
+        ctx.save();
+        ctx.translate(canvas.width/2, canvas.height/2);
+        ctx.rotate(i + Math.PI * 2 / bufferLength);
+        const hue = i * 15;
+        ctx.fillStyle = 'hsl(' + hue + ', 100%, 50%)';
+        ctx.fillRect(0, 0, barWidth, barHeight);
+        x += barWidth;
+        ctx.restore();
+    }
+}
